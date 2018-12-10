a00e5d5264ff764dc09a57c47bbb804b39f8ec9d
Keishon Ross <keishonross97@gmail.com>
2018-12-10 22:35:49 +0000

    email includes list of ordered items and total price

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,6 @@
 class UserMailer < ApplicationMailer
   def order_receipt(order)
+    @order = order
     @url  = 'http://example.com/login'
     mail(to: order.email, subject: "Jungle Order ##{order.id}")
   end
