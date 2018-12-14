d9c64d7684cf4c22b29ba0c8c72265c544525891
Keishon Ross <keishonross97@gmail.com>
2018-12-14 21:19:55 +0000

    fix bug where delete buttons on reviews would not redirect

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,20 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
+    @review = Review.new
+  end
+
+  def reviews
+    ### Idea #1:
+    # get the datas from the forms
+    # make the damn thing
+    # redirect to product page
   end
 
 end
+
+
+### Idea #2:
+# Create a revicews controller
+# change the routes.rb so that the product_reviews_path goes to that reviews ontroller's #create
+# put the shit from IDea#1 into that place?
